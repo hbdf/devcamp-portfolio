6e07eb6d1b69e46d5f0a6672c033c24f5443c328
helio <heliobduartefilho@gmail.com>
2019-04-26 15:01:05 -0300

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "asdkfasçlkfjçalkkjfçlkajsdflçkasjdfçlkkjad
+        asdfdljasçdfkjasfd
+        asdçfljaslçkfdjçasdkfjçalsdkjfçlasjfçlaksjfçlj"
+    )
+end
+
+puts "blogs posts OK"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "skills OOK"
+
+9.times do |portfolio|
+    Portfolio.create!(
+        title: "Title #{portfolio}",
+        subtitle: "Subtitle",
+        body: "lkajdflkajfdjlj",
+        main_image: "https://placehold.it/600x400",
+        thumb_image: "https://placehold.it/300x200"
+    )
+end
