39a4cb8eed64ff70b09bf746aba91ff258cf696d
Helio Bezerra Duarte Filho <heliobduartefilho@gmail.com>
2019-05-03 14:46:17 -0300

    Refactored controller portfolio params permit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,8 +13,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, 
-		technologies_attributes: [:name]))
+		@portfolio_item = Portfolio.new(portfolio_params)
 
 		respond_to do |format|
 			if @portfolio_item.save
@@ -32,7 +31,7 @@ class PortfoliosController < ApplicationController
 	def update
 		@portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolio_path(@portfolio_item.id), notice: 'Record successfully updated' }
       else
         format.html { render :edit }
@@ -51,5 +50,11 @@ class PortfoliosController < ApplicationController
 			format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully deleted.' }
       format.json { head :no_content }
     end
-  end
+	end
+	private
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body, 
+			technologies_attributes: [:name])
+	end
 end
