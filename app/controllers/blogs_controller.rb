c24587a09fa45bc2e513acbe88fcb19ceea1c283
Helio Bezerra Duarte Filho <heliobduartefilho@gmail.com>
2019-05-22 16:39:20 -0300

    Implemented blog widget for topics

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   before_action :set_title
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
   layout "blog"
@@ -97,5 +98,9 @@ class BlogsController < ApplicationController
 
     def set_title
       @page_title = "Blog | Helio"
+    end
+
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
     end
 end
